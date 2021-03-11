abe738e8c2e42fded84dc76c1381f01b157df9c9
Пархоменко Алексей <aparkhomenko@MacBook-Pro-Parhomenko.local>
2021-03-11 12:16:20 +0500

    Started our Loco framework

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,18 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by Пархоменко Алексей on 11.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Пархоменко Алексей on 11.03.2021.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSmtg() -> String {
+        return "Did some Stuff!"
+    }
+    
+}
